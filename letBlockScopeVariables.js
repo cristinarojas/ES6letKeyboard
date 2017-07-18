1b107aabe19f48f651101fa9a16d91d200dfa7ec
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-07-18 14:47:31 -0700

    Adding excercise of let keyword

---
+++ letBlockScopeVariables.js
@@ -0,0 +1,25 @@
+// let keyword are called as block scope variables. 
+// When the block scope variables are declared inside a block, they are accesible inside the block that they are defined in 
+// but no outside the block
+
+let a = 12; // Accesible globally
+
+function myFunction() {
+	console.log(a);
+
+	let b = 13; // Accesible throughout the function
+
+	if(true) {
+		let c = 14; // Accesible throughout the "if" statement
+		console.log(b);
+	}
+
+	console.log(c); // Reference Error Exception
+}
+
+myFunction();
+
+// Output
+// 12 Globally
+// 13 Accesible throughout the function
+// Reference Error Exception
